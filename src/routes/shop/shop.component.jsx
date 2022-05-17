9177c84f4103bea7db82a97dc88e8b1d3ba0f2f9
Kirill Arkhypov <k.arkhypov@gmail.com>
2022-05-17 20:49:50 +0300

    updating thunk pattern for fetching categories, using isLoading to render spinner

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,8 +2,7 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
 
 import CategoriesPreview from '../../routes/categories-preview/categories-preview.component';
 import Category from '../../routes/category/category.component';
@@ -12,12 +11,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesStartAsync());
   }, [dispatch]);
 
   return (
